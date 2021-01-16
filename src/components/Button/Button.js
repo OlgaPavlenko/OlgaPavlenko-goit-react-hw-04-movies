bb6190ef8d6711ea6c050db31056d61095e6c43f
Olga Pavlenko <olgaigpavlenko@gmail.com>
2021-01-16 23:47:10 +0200

    corrected history.push

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,12 +2,13 @@ import { useHistory } from "react-router-dom";
 
 export default function Button({ location }) {
   const history = useHistory();
+  console.log(location);
   const onBack = (evt) => {
     evt.preventDefault();
     if (location?.state?.from) {
       history.push(location.state.from);
     } else {
-      history.push("/movies");
+      history.push("/");
     }
   };
   return <button onClick={onBack}>Go Back</button>;
