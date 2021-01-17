203974dd83058f2892ba3ae3c01cfb41adb3c519
Olga Pavlenko <olgaigpavlenko@gmail.com>
2021-01-17 02:16:18 +0200

    added some markup

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -61,28 +61,31 @@ export default function Searchbar() {
           </button>
         </form>
       </header>
-
-      <ul>
-        {movies &&
-          movies.map((movie) => (
-            <li key={movie.id}>
-              <img
-                src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
-                alt={movie.title}
-              />
-              <p>
-                <Link
-                  to={{
-                    pathname: `/movies/${movie.id}`,
-                    state: { from: location },
-                  }}
-                >
-                  {movie?.title}
-                </Link>
-              </p>
-            </li>
-          ))}
-      </ul>
+      <div className={s.wrapper}>
+        <ul className={s.list}>
+          {movies &&
+            movies.map((movie) => (
+              <li key={movie.id} className={s.item}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+                  alt={movie.title}
+                  className={s.image}
+                />
+                <p className={s.title}>
+                  <Link
+                    to={{
+                      pathname: `/movies/${movie.id}`,
+                      state: { from: location },
+                    }}
+                    className={s.link}
+                  >
+                    {movie?.title}
+                  </Link>
+                </p>
+              </li>
+            ))}
+        </ul>
+      </div>
     </>
   );
 }
